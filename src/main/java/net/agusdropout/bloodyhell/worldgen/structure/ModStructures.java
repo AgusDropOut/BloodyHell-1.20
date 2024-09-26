78d7f59f68affc4f8b8555eac2401b02875b96d8
AgusDropOut <buralliagustin2003@gmail.com>
2024-09-26 20:29:22 -0300

    -INCREASED ROOM DIVERSITY ON THE MAUSOLEUM!

--- src/main/java/net/agusdropout/bloodyhell/worldgen/structure/ModStructures.java
+++ src/main/java/net/agusdropout/bloodyhell/worldgen/structure/ModStructures.java
@@ -9,13 +9,14 @@ import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureType;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModStructures {
     public static final DeferredRegister<StructureType<?>> STRUCTURES = DeferredRegister.create(Registries.STRUCTURE_TYPE, BloodyHell.MODID);
     public static final ResourceKey<Structure> CATACOMBS = ResourceKey.create(Registries.STRUCTURE, new ResourceLocation(BloodyHell.MODID, "portal"));
     //public static final ResourceKey<Structure> BLOOD_LIGHTHOUSE = ResourceKey.create(Registries.STRUCTURE, new ResourceLocation(BloodyHell.MODID, "blood_lighthouse"));
     public static final ResourceKey<Structure> MAUSOLEUM = ResourceKey.create(Registries.STRUCTURE, new ResourceLocation(BloodyHell.MODID, "mausoleum"));
-
+    public static final RegistryObject<StructureType<BiggerJigsawStructure>> BIGGER_JIGSAW = STRUCTURES.register("bigger_jigsaw", () -> () -> BiggerJigsawStructure.CODEC);
     public static void register(IEventBus eventBus){
         STRUCTURES.register(eventBus);
     }
